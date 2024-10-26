63f624383b2a5143835e41f1bed586e297bc3182
abhishek dm <abhishekd301@gmail.com>
2024-10-26 19:21:45 +0530

    chore: Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  //https://vite.dev/config/shared-options#css-preprocessoroptions
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
